a926c6cacb53c517c1936e76657d8d47e4d68d81
sultanxdev <sultancodess@gmail.com>
2026-02-21 00:33:45 +0530

    making good navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -8,7 +8,7 @@ import { ThemeToggleButton } from './ThemeSwitch';
 
 export default function Navbar() {
   return (
-    <Container className="sticky top-4 z-50 rounded-full border border-white/10 bg-background/40 py-2 shadow-lg backdrop-blur-md !max-w-[680px]">
+    <Container className="sticky top-4 z-50 rounded-full border border-white/10 bg-background/40 py-2 shadow-lg backdrop-blur-md !max-w-[702px]">
       <div className="flex items-center justify-between gap-12 px-4">
         <div className="flex items-center gap-6">
           {navbarConfig.navItems.map((item) => (
